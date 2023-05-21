02d8ecc72b96718c42669f80980d65d13bb784ff
Семён Бабушкин <babushkin.semyon@gmail.com>
2023-05-21 18:39:28 +0800

    pg settings changes

--- src/migrations/20210503202829_create_uuid_extension.ts
+++ src/migrations/20210503202829_create_uuid_extension.ts
@@ -2,10 +2,10 @@ import * as Knex from 'knex';
 
 export async function up(knex: Knex): Promise<any> {
   await knex.raw('CREATE EXTENSION IF NOT EXISTS "uuid-ossp"');
-  await knex.raw('CREATE EXTENSION IF NOT EXISTS postgis');
+  // await knex.raw('CREATE EXTENSION IF NOT EXISTS postgis');
 }
 
 export async function down(knex: Knex): Promise<any> {
   await knex.raw('DROP EXTENSION IF EXISTS "uuid-ossp"');
-  await knex.raw('DROP EXTENSION IF EXISTS postgis');
+  // await knex.raw('DROP EXTENSION IF EXISTS postgis');
 }
