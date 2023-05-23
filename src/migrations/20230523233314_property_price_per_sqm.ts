dee27afe253f67276883632582a2860490cc7f98
Семён Бабушкин <babushkin.semyon@gmail.com>
2023-05-23 23:37:06 +0800

    property price per sqm

---
+++ src/migrations/20230523233314_property_price_per_sqm.ts
@@ -0,0 +1,15 @@
+import * as Knex from 'knex';
+
+export async function up(knex: Knex): Promise<void> {
+  await knex.raw(`
+    CREATE FUNCTION property_price_per_sqm(property property) RETURNS double precision AS $$
+      SELECT ROUND((COALESCE(property.price_idr, 0)/COALESCE(NULLIF(property.building_size, 0), 1))::numeric, 2);
+    $$ LANGUAGE sql STABLE;
+  `);
+}
+
+export async function down(knex: Knex): Promise<void> {
+  await knex.raw(`
+    DROP FUNCTION property_price_per_sqm;
+  `);
+}
